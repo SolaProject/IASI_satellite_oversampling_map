4045090132b9267437bded183c68ba67c418dafa
SolaProject <sola1211582551@outlook.com>
2024-08-07 14:58:37 +0800

    v0.0.2

--- bundle.js
+++ bundle.js
@@ -908,6 +908,9 @@ var staInfo = /*#__PURE__*/Object.freeze({
   type: type
 });
 
+const cmap = "jet";
+const reverse = false;
+
 //加载基础图层
 var ESRI = L.tileLayer(
     "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
@@ -924,11 +927,15 @@ var gridLayer_am = L.vectorGrid.protobuf(
     {
         rendererFactory: L.canvas.tile,
         attribution: "© Sola",
+        interactive: true,
+        getFeatureId: function(feature) {
+            return feature.properties.id;
+        },
         vectorTileLayerStyles: {
             col_am: function(properties) {
                 return {
                     fill: true,
-                    fillColor: evaluate_cmap_hex(Math.abs(properties.data)/700, "jet", false),
+                    fillColor: evaluate_cmap_hex(Math.abs(properties.data)/1000, cmap, reverse),
                     fillOpacity: 0.5,
                     stroke: false, //不显示网格边框线
                     weight: 0,
@@ -937,6 +944,7 @@ var gridLayer_am = L.vectorGrid.protobuf(
         },
         maxNativeZoom: 13,
         maxZoom: 22,
+        // minZoom: 7,
     },
 );
 
@@ -946,11 +954,15 @@ var gridLayer_pm = L.vectorGrid.protobuf(
     {
         rendererFactory: L.canvas.tile,
         attribution: "© Sola",
+        interactive: true,
+        getFeatureId: function(feature) {
+            return feature.properties.id;
+        },
         vectorTileLayerStyles: {
             col_pm: function(properties) {
                 return {
                     fill: true,
-                    fillColor: evaluate_cmap_hex(Math.abs(properties.data)/1000, "jet", false),
+                    fillColor: evaluate_cmap_hex(Math.abs(properties.data)/1000, cmap, reverse),
                     fillOpacity: 0.5,
                     stroke: false, //不显示网格边框线
                     weight: 0,
@@ -959,6 +971,7 @@ var gridLayer_pm = L.vectorGrid.protobuf(
         },
         maxNativeZoom: 13,
         maxZoom: 22,
+        // minZoom: 7,
     },
 );
 
@@ -1032,7 +1045,101 @@ const overlays = {
 //创建地图
 const map = L.map('map', {
     center: [40.9828, 120.141],
-    zoom: 5,
+    zoom: 7,
     layers: [ESRI, gridLayer_am, gridLayer_line, station]
 });
+//鼠标悬浮显示网格信息
+var info = L.control();
+info.onAdd = function (map) {
+    this._div = L.DomUtil.create('div', 'info'); 
+    this.update();
+    return this._div;
+};
+info.update = function (props) {
+    this._div.innerHTML = '<b>Data indicators</b>' + '<br />' +
+        (props ? '氨气柱浓度: ' + props.data + ' mol/km<sup>2</sup>' + '<br />'
+        : '鼠标悬停网格区域显示相关信息');
+};
+info.addTo(map);
 L.control.layers(baseLayers, overlays).addTo(map);
+
+
+function highlightFeature(e) {
+    //更新信息
+    var props = e.layer.properties;
+    info.update(props);
+}
+function resetHighlight(e) {
+    //重置信息
+    info.update();
+}
+gridLayer_am.on({
+    mouseover: highlightFeature,
+    mouseout: resetHighlight,
+});
+gridLayer_pm.on({
+    mouseover: highlightFeature,
+    mouseout: resetHighlight,
+});
+
+// station.on('click', function (e) { // this line and below
+//     L.popup()
+//         .setContent(`Id: ${e.layer.properties.index}`)
+//         .setLatLng(e.latlng)
+//         .openOn(map);
+// });
+
+var lonlat_info = L.control({position: 'bottomright'});
+lonlat_info.onAdd = function (map) {
+    this._div = L.DomUtil.create('div', 'info'); 
+    // this.update();
+    return this._div;
+};
+lonlat_info.update = function (lon, lat) {
+    this._div.innerHTML = `
+    <table>
+      <tbody>
+        <tr>
+          <td><b>Lon:</b></td>
+          <td>${lon.toFixed(3)}</td>
+        </tr>
+        <tr>
+          <td><b>Lat:</b></td>
+          <td>${lat.toFixed(3)}</td>
+        </tr>
+      </tbody>
+    </table>
+    `;
+};
+
+lonlat_info.addTo(map);
+function update_lonlat(e) {
+    var lon = e.latlng.lng;
+    var lat = e.latlng.lat;
+    lonlat_info.update(lon, lat);
+}
+map.on({
+    mousemove: update_lonlat
+});
+
+//添加常见控件
+L.control.scale({maxWidth:100,metric:true,imperial:false}).addTo(map); // 比例尺控件
+
+//添加图例控件
+var legend = L.control({position: 'bottomright'});
+
+legend.onAdd = function (map) {
+
+    var div = L.DomUtil.create('div', 'info legend'),
+        grades = [0, 100, 200, 300, 400, 500, 600, 800, 900, 1000];
+
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + evaluate_cmap_hex(grades[i]/1000, cmap, reverse) + '"></i> ' + grades[i] + '<br>';
+    }
+
+    return div;
+};
+
+legend.addTo(map);
